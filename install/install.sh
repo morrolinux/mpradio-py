55493478166de28990f4364d911199f0511eb48b
morrolinux <morrolinux@gmail.com>
2019-04-12 12:39:06 +0200

    beautify installer

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,13 @@
+sudo apt-get -y install git libsndfile1-dev libbluetooth-dev bluez pi-bluetooth python-gobject python-gobject-2 bluez-tools sox ffmpeg libsox-fmt-mp3 python-dbus bluealsa obexpushd python3-rpi.gpio 
+
+
 for f in $(ls -d */)
 do 
 	sudo cp -R --parents $f /
 done
+
+for f in $(ls etc/systemd/system/*.service)
+do 
+	sudo systemctl enable $(basename $f)
+done
+
