167889d9fbecd20a262c9c42907cb1a74431b815
morrolinux <morrolinux@gmail.com>
2019-04-13 18:07:44 +0200

    to be tested

--- src/bluetooth_player.py
+++ src/bluetooth_player.py
@@ -1,17 +1,23 @@
+import dbus
 from player import Player
 import subprocess
 from fcntl import fcntl, F_GETFL, F_SETFL
 from os import O_NONBLOCK
 import threading
+from rds import RdsUpdater
+import time
 
 
 class BtPlayer(Player):
 
     __bt_addr = None
     __cmd_arr = None
+    __rds_updater = None
+    __now_playing = None
 
     def __init__(self, bt_addr):
         super().__init__()
+        self.__rds_updater = RdsUpdater()
         self.__bt_addr = bt_addr
         self.__cmd_arr = ["sudo", "dbus-send", "--system", "--type=method_call", "--dest=org.bluez", "/org/bluez/hci0/dev_"
                           + bt_addr.replace(":", "_").upper() + "/player0", "org.bluez.MediaPlayer1.Pause"]
@@ -34,6 +40,29 @@ class BtPlayer(Player):
         # set the player to non-blocking output:
         flags = fcntl(self.stream.stdout, F_GETFL)  # get current stdout flags
         fcntl(self.stream.stdout, F_SETFL, flags | O_NONBLOCK)
+
+        self.__rds_updater.run()
+
+        # Wait until process terminates
+        while self.stream.poll() is None:
+            self.get_now_playing()
+            self.__rds_updater.set(self.__now_playing)
+            time.sleep(5)
+
+    def get_now_playing(self):
+        try:
+            bus = dbus.SystemBus()
+            player = bus.get_object("org.bluez", "/org/bluez/hci0/dev_" + self.__bt_addr.replace(":", "_").upper()
+                                    + "/player0")
+            BT_Media_iface = dbus.Interface(player, dbus_interface="org.bluez.MediaPlayer1")
+            BT_Media_probs = dbus.Interface(player, "org.freedesktop.DBus.Properties")
+            probs = BT_Media_probs.GetAll("org.bluez.MediaPlayer1")
+
+            self.__now_playing["title"] = probs["Track"]["Title"]
+            self.__now_playing["artist"] = probs["Track"]["Artist"]
+            self.__now_playing["album"] = probs["Track"]["Album"]
+        except dbus.DBusException:
+            pass
 
     def pause(self):
         self.__cmd_arr[len(self.__cmd_arr) - 1] = "org.bluez.MediaPlayer1.Pause"
