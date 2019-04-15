d37f73c5eeb1dcc85ae0aed4e7ede2719236cd08
morrolinux <morrolinux@gmail.com>
2019-04-15 12:11:00 +0200

    added compression

--- src/encoder.py
+++ src/encoder.py
@@ -6,15 +6,18 @@ from os import O_NONBLOCK
 class Encoder:
 
     stream = None
+    __sox_cmd = ["sox", "-t", "raw", "-G", "-b", "16", "-e", "signed",
+                 "-c", "2", "-r", "44100", "-", "-t", "wav", "-"]
+    __sox_treble = ["treble", "-6"]
+    __sox_compression = ["compand", "0.3,1", "6:-70,-60,-20", "-5", "-90", "0.2"]
 
     def __init__(self):
-        pass
         # TODO: read from settings
+        self.__sox_cmd.extend(self.__sox_treble)
+        self.__sox_cmd.extend(self.__sox_compression)
 
     def run(self):
-        self.stream = subprocess.Popen(["sox", "-t", "raw", "-G", "-b", "16", "-e", "signed",
-                                        "-c", "2", "-r", "44100", "-", "-t", "wav", "-", "treble", "-6"],
-                                       stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+        self.stream = subprocess.Popen(self.__sox_cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
                                        stderr=subprocess.PIPE, bufsize=0)    # -1
         # set the encoder to non-blocking output:
         flags = fcntl(self.stream.stdout, F_GETFL)  # get current stdout flags
