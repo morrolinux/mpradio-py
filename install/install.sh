95e2b66dfb2cf53b2d94e21f2114c970383715c3
morrolinux <morrolinux@gmail.com>
2019-04-15 13:23:41 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,9 @@
-sudo apt-get -y install git libsndfile1-dev libbluetooth-dev bluez pi-bluetooth python-gobject python-gobject-2 bluez-tools sox ffmpeg libsox-fmt-mp3 python-dbus bluealsa obexpushd python3-rpi.gpio python3-mutagen
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+apt-get -y install git libsndfile1-dev libbluetooth-dev bluez pi-bluetooth python-gobject python-gobject-2 bluez-tools sox ffmpeg libsox-fmt-mp3 python-dbus bluealsa obexpushd python3-rpi.gpio python3-mutagen
 
 
 # install requirements as a dir. structure
@@ -27,6 +32,23 @@ make -j $(nproc)
 
 cp /usr/local/src/PiFmAdv/src/pi_fm_adv /usr/local/bin/pi_fm_adv
 
+# set gpu_freq needed by PiFmAdv
 if [[ $(grep "gpu_freq=250" /boot/config.txt) == "" ]]; then 	
     echo "gpu_freq=250" >> /boot/config.txt	
 fi
+
+# Final configuration and perms...
+FSTAB="/etc/fstab"
+fstabline=$(grep "pirateradio" $FSTAB -n|cut -d: -f1)
+if [[ $fstabline == "" ]]; then
+	echo "/dev/sda1    /pirateradio    vfat    defaults,rw,nofail 0   0" >> $FSTAB
+fi
+
+# usermod -a -G lp pi
+
+# set hostname
+echo PRETTY_HOSTNAME=mpradio > /etc/machine-info
+# avoid recompilation (by need2recompile.service) after first install 
+cp -f /sys/firmware/devicetree/base/model /etc/lastmodel
+
+sleep 5 && reboot
