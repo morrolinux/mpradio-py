cf6e33a26e183ad84dbfd4bab8caf1afe9c32fd1
morrolinux <morrolinux@gmail.com>
2019-04-19 17:06:35 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -3,6 +3,8 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+ln -s src/mpradio.py /home/pi/mpradio.py
+
 apt-get -y install git libsndfile1-dev libbluetooth-dev bluez pi-bluetooth python-gobject python-gobject-2 bluez-tools sox ffmpeg libsox-fmt-mp3 python-dbus bluealsa obexpushd python3-rpi.gpio python3-mutagen python3-dbus python3-pip
 
 # needed for rfcomm bluetooth interface
