985f2230f63ba7b9cb814be9b44ada8b4f9c6ba0
morrolinux <morrolinux@gmail.com>
2019-04-24 14:21:20 +0200

    Revert "generating audio stream test"
    This reverts commit 0c1df1c80c56bb572a2a9d62f2b9d8e99d04d661.

--- src/player.py
+++ src/player.py
@@ -1,31 +1,16 @@
 from abc import abstractmethod
 from media import MediaInfo, MediaControl
+import subprocess
 import time
-from mp_io import MpradioIO
 
 
 class Player(MediaControl, MediaInfo):
 
     CHUNK = 2048
     SLEEP_TIME = 0.035
-
     out = None
     event = None    # TODO: maybe delete?
     _tmp_stream = None
-    silence_sound = None
-    __wav_header = (b'RIFF\xff\xff\xff\xffWAVEfmt \x10\x00\x00\x00\x01\x00\x02\x00D\xac\x00\x00\x10\xb1'
-                    b'\x02\x00\x04\x00\x10\x00LIST\x1a\x00\x00\x00INFOISFT\x0e\x00\x00\x00Lavf58.20.100'
-                    b'\x00data\xff\xff\xff\xff\x00')
-    __wav_silence = b'\x00'
-    __wav_end = b'xee=\x11'
-
-    def __init__(self):
-        # Craft silence sound
-        self.silence_sound = MpradioIO()
-        self.silence_sound.write(self.__wav_header)
-        for _ in range(0, 1000):
-            self.silence_sound.write(self.__wav_silence)
-        self.silence_sound.write(self.__wav_end)
 
     @abstractmethod
     def playback_position(self):
@@ -33,5 +18,7 @@ class Player(MediaControl, MediaInfo):
 
     def silence(self, silence_time=0.8):
         self._tmp_stream = self.out
-        self.out = self.silence_sound
+        self.out = subprocess.Popen(["sox", "-n", "-r", "48000", "-b", "16", "-c", "1", "-t", "wav", "-",
+                                               "trim", "0", str(silence_time)],
+                                              stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout
         time.sleep(silence_time)
