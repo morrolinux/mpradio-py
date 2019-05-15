54f457319f77832d979eb9f6072b9e1368604e0f
morrolinux <morrolinux@gmail.com>
2019-05-15 20:58:32 +0200

    first attempt

--- src/bluetooth_player.py
+++ src/bluetooth_player.py
@@ -1,11 +1,13 @@
 import dbus
 from player import Player
 import subprocess
-from fcntl import fcntl, F_GETFL, F_SETFL
-from os import O_NONBLOCK
+import av
+from mp_io import MpradioIO
 import threading
 from rds import RdsUpdater
 import time
+import psutil
+import threading
 
 
 class BtPlayer(Player):
@@ -14,11 +16,15 @@ class BtPlayer(Player):
     __cmd_arr = None
     __rds_updater = None
     __now_playing = None
+    __out = None
+    output_stream = None
+    __terminating = False
 
     def __init__(self, bt_addr):
         super().__init__()
         self.__rds_updater = RdsUpdater()
         self.__bt_addr = bt_addr
+        self._tmp_stream = None
         self.__cmd_arr = ["sudo", "dbus-send", "--system", "--type=method_call", "--dest=org.bluez", "/org/bluez/hci0/dev_"
                           + bt_addr.replace(":", "_").upper() + "/player0", "org.bluez.MediaPlayer1.Pause"]
 
@@ -35,23 +41,90 @@ class BtPlayer(Player):
     def __run(self):
         print("playing bluetooth:", self.__bt_addr)
         dev = "bluealsa:HCI=hci0,DEV="+self.__bt_addr
-        self.stream = subprocess.Popen(["arecord", "-D", dev, "-f", "cd", "-c", "2"],
-                                       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        # set the player to non-blocking output:
-        flags = fcntl(self.stream.stdout, F_GETFL)  # get current stdout flags
-        fcntl(self.stream.stdout, F_SETFL, flags | O_NONBLOCK)
-        self.output_stream = self.stream.stdout
-
-        self.ready.set()
-
         self.__rds_updater.run()
+        while not self.__terminating:
+            self.play(dev)
+            time.sleep(0.5)
 
-        # Wait until process terminates
-        while self.stream.poll() is None:
-            self.get_now_playing()
-            self.__rds_updater.set(self.__now_playing)
-            time.sleep(3)
+    def play(self, device):
+        self._tmp_stream = None
+
+        # open input device
+        try:
+            input_container = av.open(device, format="alsa")
+
+            audio_stream = None
+            for i, stream in enumerate(input_container.streams):
+                if stream.type == 'audio':
+                    audio_stream = stream
+                    break
+
+            if not audio_stream:
+                print("audio stream not found")
+                return
+
+        except av.AVError:
+            print("Can't open input stream for device", device)
+            return
+
+        # open output stream
+        self.__out = MpradioIO()
+        self.output_stream = self.__out       # link for external access
+        out_container = av.open(self.__out, 'w', 'wav')
+        out_stream = out_container.add_stream(codec_name='pcm_s16le', rate=44100)
+
+        # transcode input to wav
+        for i, packet in enumerate(input_container.demux(audio_stream)):
+            try:
+                for frame in packet.decode():
+                    frame.pts = None
+                    out_pack = out_stream.encode(frame)
+                    if out_pack:
+                        out_container.mux(out_pack)
+                    else:
+                        print("out_pack is None")
+            except av.AVError:
+                print("Error during playback for:", device)
+                return
+
+            # stop transcoding if we receive termination signal
+            if self.__terminating:
+                print("termination signal received")
+                break
+
+            if i == 10:
+                self.ready.set()
+
+            # avoid CPU saturation on single-core systems
+            if psutil.cpu_percent() > 95:
+                time.sleep(0.01)
+
+        # transcoding terminated. Flush output stream
+        try:
+            while True:
+                out_pack = out_stream.encode(None)
+                if out_pack:
+                    out_container.mux(out_pack)
+                else:
+                    break
+        except ValueError:
+            print("skipping flush...")
+            return
+
+        # close output container and tell the buffer no more data is coming
+        out_container.close()
+        self.__out.set_write_completed()
+        print("transcoding finished.")
+
+        # TODO: check if this and the above is really needed when playing a device
+        # wait until playback (buffer read) terminates; catch signals meanwhile
+        while not self.__out.is_read_completed():
+            if self.__skip.is_set():
+                self.__skip.clear()
+                break
+            if self.__terminating:
+                break
+            time.sleep(0.2)
 
     def get_now_playing(self):
         try:
@@ -95,8 +168,8 @@ class BtPlayer(Player):
 
     def stop(self):
         self.silence()
-        self.stream.kill()
         self.__rds_updater.stop()
+        self.__terminating = True
         print("bluetooth player stopped")
 
     def song_name(self):
