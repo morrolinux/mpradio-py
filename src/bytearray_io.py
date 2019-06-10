5e3a6620b1519d0ff5bf0dd2fe975eabb6793901
morrolinux <morrolinux@gmail.com>
2019-06-10 17:26:41 +0200

    second attempt

--- src/bytearray_io.py
+++ src/bytearray_io.py
@@ -1,31 +1,60 @@
 from typing import Union
+import time
 
 
 class BytearrayIO:
 
     def __init__(self):
-        self.buf_size = 1024 * 10000
+        self.buf_size = 1024 * 1000  # 1M
         self.buf = bytearray(self.buf_size)
         self.mv = memoryview(self.buf)
         self.last_r = 0
         self.last_w = 0
+        self.wrap_around = False
+        self.wrap_around_at = self.buf_size
         self.__write_completed = False
 
     def silence(self, silent=True):
         pass
 
     def read(self, size=1024):
-        print("read", size)  # non è effettivamente quanto viene letto ma quanto viene chiesto
-        start = self.last_r
-        end = min(self.last_r + size, self.last_w)  # don't read more data than available
-        self.last_r = end
-        return self.buf[start:end]
+        while True:
+
+            if self.last_r == self.wrap_around_at:
+                self.last_r = 0
+                print("read wrap around at", self.wrap_around_at)
+
+            start = self.last_r
+            available = self.last_w - self.last_r
+
+            # write wrap around detection and handling
+            if available < 0:
+                available = self.wrap_around_at - self.last_r
+
+            # read what's possible
+            rs = min(size, available)
+
+            if rs == 0:
+                print("buffer underrun")
+                time.sleep(0.008)
+                continue
+
+            end = start + rs
+            self.last_r = end
+            return self.buf[start:end]
 
     def write(self, b: Union[bytes, bytearray]):
         size = len(b)
-        print("wrote", size)
+
+        # write wrap around
+        if self.last_w + size > self.buf_size:
+            self.wrap_around_at = self.last_w
+            self.last_w = 0
+            self.wrap_around = True
+            print("write wrap around at", self.wrap_around_at)
+
         start = self.last_w
-        self.last_w = self.last_w + size
+        self.last_w = start + size
         try:
             self.mv[start:self.last_w] = b
         except ValueError:
@@ -44,4 +73,8 @@ class BytearrayIO:
         self.last_r = 0
 
     def stop(self):
+        pass
+
+    # IoBase dummy interface implementation for pyav
+    def seek(self, offset: int, whence: int = ...):
         pass
