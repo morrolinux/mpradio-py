a58463408419bc0835974fa380f7a1dc0bc25512
Morrolinux <morrolinux@gmail.com>
2019-06-11 11:23:49 +0200

    wrap around works fine

--- src/bytearray_io.py
+++ src/bytearray_io.py
@@ -8,72 +8,88 @@ class BytearrayIO:
         self.buf_size = 1024 * 1000  # 1M
         self.buf = bytearray(self.buf_size)
         self.mv = memoryview(self.buf)
-        self.last_r = 0
-        self.last_w = 0
-        self.wrap_around = False
-        self.wrap_around_at = self.buf_size
+        self.__last_r = 0
+        self.__last_w = 0
+        self.__wrap_around_at = self.buf_size
         self.__write_completed = False
+        self.__available = 0
+        self.__terminating = False
 
     def silence(self, silent=True):
         pass
 
     def read(self, size=1024):
+
         while True:
 
-            if self.last_r == self.wrap_around_at:
-                self.last_r = 0
-                print("read wrap around at", self.wrap_around_at)
+            if self.__terminating:
+                break
 
-            start = self.last_r
-            available = self.last_w - self.last_r
-
-            # write wrap around detection and handling
-            if available < 0:
-                available = self.wrap_around_at - self.last_r
-
-            # read what's possible
-            rs = min(size, available)
-
-            if rs == 0:
-                print("buffer underrun")
-                time.sleep(0.008)
+            # buffer underrun protection by checking total amount of available data
+            # note that this is an absolute value and doesn't consider wrap around(s)
+            if self.__available <= 0:
+                time.sleep(0.01)
+                # print("buffer underrun. available =", self.available)
                 continue
 
+            # reader wrap around when reaching last written byte
+            if self.__last_r == self.__wrap_around_at:
+                self.__last_r = 0
+                print("read wrap around at", self.__wrap_around_at)
+
+            start = self.__last_r
+
+            # available data in the buffer (wrap around aware)
+            # if margin > 0, the read head is following the write head: |--r--w--|
+            # if margin < 0, the write head has wrapped around and is following/reaching the read head: |--w--r--|
+            margin = self.__last_w - start
+
+            # if the writer already wrapped around we still need to consume the buffer until the wrap around point
+            if margin < 0:
+                margin = self.__wrap_around_at - start
+                # print("writer wrap around detected")
+
+            # read what's available (could be lower than requested size)
+            rs = min(size, margin)
+
+            self.__available -= rs
+            # print("now available:", self.available)
+
             end = start + rs
-            self.last_r = end
+            self.__last_r = end
             return self.buf[start:end]
 
     def write(self, b: Union[bytes, bytearray]):
         size = len(b)
 
         # write wrap around
-        if self.last_w + size > self.buf_size:
-            self.wrap_around_at = self.last_w
-            self.last_w = 0
-            self.wrap_around = True
-            print("write wrap around at", self.wrap_around_at)
+        if self.__last_w + size > self.buf_size:
+            self.__wrap_around_at = self.__last_w
+            self.__last_w = 0
+            print("write wrap around at", self.__wrap_around_at)
 
-        start = self.last_w
-        self.last_w = start + size
+        start = self.__last_w
+        self.__last_w = start + size
+        self.__available += size
         try:
-            self.mv[start:self.last_w] = b
+            self.mv[start:self.__last_w] = b
         except ValueError:
-            print("Value error. len(buf) = ", len(self.buf), "last_w = ", self.last_w)
+            print("Value error. len(buf) = ", len(self.buf), "last_w = ", self.__last_w)
 
     def set_write_completed(self):
         self.__write_completed = True
 
     def is_read_completed(self):
-        if self.last_w > 0 and self.last_w == self.last_r and self.__write_completed:
+        if self.__last_w > 0 and self.__last_w == self.__last_r and self.__write_completed:
             return True
         else:
             return False
 
     def seek_to_start(self):
-        self.last_r = 0
+        self.__last_r = 0
 
     def stop(self):
-        pass
+        self.__terminating = True
 
     # IoBase dummy interface implementation for pyav
     def seek(self, offset: int, whence: int = ...):
