d4ae8f8d7b107bf9ab5691d10475edfe8362742b
Morrolinux <morrolinux@gmail.com>
2019-10-04 09:03:57 +0200

    roswitch should umount tmpfs overlay filesystem when remounting rw

--- install/usr/local/bin/roswitch.sh
+++ install/usr/local/bin/roswitch.sh
@@ -7,22 +7,25 @@ fi
 
 if [[ $1 == "ro" ]]
 then
-        sudo sed -i.bak '/^PARTUUID/ s/defaults/defaults,ro/' /etc/fstab
-        sudo sed -i.bak 's/rootwait *$/rootwait noswap ro/' /boot/cmdline.txt
+        sed -i.bak '/^PARTUUID/ s/defaults/defaults,ro/' /etc/fstab
+        sed -i.bak 's/rootwait *$/rootwait noswap ro/' /boot/cmdline.txt
 
         if [[ $(sudo grep "var" /etc/fstab) == "" ]]
         then
-                sudo echo "tmpfs /var tmpfs noatime 0 0" >> /etc/fstab
+                echo "tmpfs /var tmpfs noatime 0 0" >> /etc/fstab
         fi
 
         echo "You need to reboot to make this effective."
 elif [[ $1 == "rw" ]]
 then
-        sudo mount -o remount,rw /
-        sudo mount -o remount,rw /boot
-        sudo sed -i.bak '/^PARTUUID/ s/defaults,ro/defaults/' /etc/fstab
-        sudo sed -i.bak 's/rootwait noswap ro*$/rootwait/' /boot/cmdline.txt
-        sudo umount -l /var/
-	sudo sed -i '/tmpfs \/var tmpfs noatime 0 0/d' /etc/fstab
+	cd /
+	umount /home/pi		# umount overlay tmpfs rw filesystem if present
+        mount -o remount,rw /
+        mount -o remount,rw /boot
+        sed -i.bak '/^PARTUUID/ s/defaults,ro/defaults/' /etc/fstab
+        sed -i.bak 's/rootwait noswap ro*$/rootwait/' /boot/cmdline.txt
+        umount -l /var/
+	sed -i '/tmpfs \/var tmpfs noatime 0 0/d' /etc/fstab
         echo "RW effective now!"
+	cd -
 fi
