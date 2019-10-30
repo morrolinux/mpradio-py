a73f935d922f0664351a28ea505708b2d729945e
morrolinux <morrolinux@gmail.com>
2019-10-30 21:35:29 +0100

    mounting /etc rw overlay + adequate tmpfs sizing

--- install/usr/local/bin/rw-overlay.sh
+++ install/usr/local/bin/rw-overlay.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-SIZE=150m
+getSize(){ 
+	size=$(sudo du -s --block-size M ${1} |cut -dM -f1)
+	size=$(($size + 5))
+	echo ${size}m
+}
+
+# SIZE=150m
 
 # mount /tmp in tmpfs
-mount -t tmpfs -o size=$SIZE tmpfs /tmp
+mount -t tmpfs -o size=$(getSize /tmp) tmpfs /tmp
 
 # mount /home/pi in tmpfs after copying its content
 cd /
 mkdir /tmp/home
 cp -Rfp /home/pi/.* /tmp/home/
 cp -Rfp /home/pi/mpradio* /tmp/home/
-
-mount -t tmpfs -o size=$SIZE tmpfs /home/pi
-
+mount -t tmpfs -o size=$(getSize /home/pi/mpradio) tmpfs /home/pi
 cp -Rfp /tmp/home/.* /home/pi/
 cp -Rfp /tmp/home/mpradio* /home/pi/
+cd /home/pi
+
+# mount /etc in tmpfs
+cd /
+mkdir /tmp/etc
+cp -Rfp /etc/* /tmp/etc/
+mount -t tmpfs -o size=$(getSize /etc) tmpfs /etc
+cp -Rfp /tmp/etc/* /etc/
 cd /home/pi
 
 # mount /pirateradio in tmpfs if no external rw filesystem is mounted
